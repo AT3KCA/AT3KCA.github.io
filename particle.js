8aec4f0caad2c074ce56c086a51a1222e3621b94
AT3KCA <biliat3k@163.com>
2024-08-31 18:01:51 +0800

    Update

--- particle.js
+++ particle.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
+let isDraw = getIsDraw();
 
 ctx.fillStyle = '#FFFFFF';
 
@@ -9,11 +10,38 @@ let particles = [];
 let mouse = {
     x: -500,
     y: -500
-}
+};
 document.addEventListener("mousemove", function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
 });
+
+let btn = document.getElementById("render");
+if (btn != null) {
+    btn.addEventListener("click", function (event) {
+        setIsDraw();
+    });
+}
+
+window.addEventListener("resize", function (event) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
+function setIsDraw() {
+    document.cookie = "isDraw=" + !isDraw;
+    isDraw = getIsDraw();
+}
+function getIsDraw() {
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.startsWith('isDraw=')) {
+            return cookie.substring('isDraw='.length) === "true";
+        }
+    }
+    return true;
+}
 
 class particle {
     constructor() {
@@ -43,11 +71,14 @@ class particle {
     }
 
     draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
-        ctx.fill();
+        if (isDraw) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+            ctx.fill();
+        }
     }
 }
+
 function drawLineP(x1, y1, x2, y2) {
     let dx = x1 - x2;
     let dy = y1 - y2;
@@ -66,7 +97,7 @@ function drawLineM(x1, y1, mx, my) {
     let dx = x1 - mx;
     let dy = y1 - my;
     let squareDistance = dx * dx + dy * dy;
-    if (squareDistance < 175 * 175) {
+    if (squareDistance < 10000) {
         ctx.beginPath();
         ctx.moveTo(x1, y1);
         ctx.lineTo(mx, my);
@@ -81,23 +112,27 @@ function init() {
         particles.push(new particle())
     }
 }
-let p1,p2;
+
+let p1, p2;
+
 function ani() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i in particles) {
         p1 = particles[i];
         p1.update();
         p1.draw();
-        drawLineM(p1.x,p1.y,mouse.x,mouse.y);
-        for (let j in particles) {
-            p2 = particles[j];
-            if (p1 === p2){
-                continue;
+        drawLineM(p1.x, p1.y, mouse.x, mouse.y);
+        if (isDraw) {
+            for (let j in particles) {
+                p2 = particles[j];
+                if (p1 === p2) {
+                    continue;
+                }
+                drawLineP(p1.x, p1.y, p2.x, p2.y);
             }
-            drawLineP(p1.x,p1.y,p2.x,p2.y);
         }
     }
 }
 
 init();
-setInterval(ani, 20);
+setInterval(ani, 5);
