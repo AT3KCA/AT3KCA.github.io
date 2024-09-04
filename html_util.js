d40d0241eab70d219900421de0884e733cb41144
AT3KCA <at3kgamesoft@163.com>
2024-09-04 22:22:00 +0800

    update

--- html_util.js
+++ html_util.js
@@ -1,24 +1,4 @@
-function renderBox(artist_url,artist, author_url,author, imgSrc, type, idea) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-
-    const h4 = document.createElement('h4');
-    h4.innerHTML = `<a href="${artist_url}">${artist}</a> - <a href="${author_url}">${author}</a>`;
-
-    const pType = document.createElement('p');
-    pType.textContent = type;
-
-    const img = document.createElement('img');
-    img.src = imgSrc;
-    img.alt = "如果你看到了我就说明图片似了";
-    let textS = idea.replaceAll("\n","</p><p>");
-    const pIdea = document.createElement('p');
-    pIdea.textContent = textS;
-
-    box.appendChild(h4);
-    box.appendChild(pType);
-    box.appendChild(img);
-    box.appendChild(pIdea);
-
-    document.body.appendChild(box);
-}
+let down_btn = document.getElementById("down");
+down_btn.addEventListener("click",function (){
+    window.scrollTo(0);
+});
