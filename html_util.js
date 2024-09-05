31763ceefc49b9ec1647d37de4a014d4d1fd91fb
AT3KCA <at3kgamesoft@163.com>
2024-09-05 19:30:00 +0800

    Big Update

--- html_util.js
+++ html_util.js
@@ -1,6 +1,41 @@
 window.addEventListener('load', function() {
-    const down_btn = document.getElementById("down");
+    const down_btn = document.getElementById("down")
+    if (down_btn == null){
+        return
+    }
     down_btn.addEventListener("click", function (){
-        window.scrollTo(0, document.body.scrollHeight);
+        window.scrollTo(0, document.body.scrollHeight)
     });
 });
+
+
+
+let inp
+let count = 0;
+let timer;
+window.addEventListener('load', function() {
+    inp = document.getElementById('in');
+    if (!inp){
+        return
+    }
+    inp.addEventListener('click', function () {
+        count++;
+        if (count >= 6){
+            window.location.href = "the-hidden.html"
+        }
+        if (!timer) {
+            timer = setTimeout(() => {
+                count = 0;
+                timer = null;
+            }, 2000);
+        }
+    });
+
+});
+
+
+
+function msg_render() {
+    const msgDiv = document.getElementById('msg')
+    msgDiv.style.display = 'block'
+}
