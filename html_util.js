2788b686b4d6504bfb177321eaae544e8add0a60
AT3KCA <at3kgamesoft@163.com>
2024-09-05 18:40:09 +0800

    update

--- html_util.js
+++ html_util.js
@@ -1,4 +1,6 @@
-let down_btn = document.getElementById("down");
-down_btn.addEventListener("click",function (){
-    window.scrollTo(0, document.body.scrollHeight);
+window.addEventListener('load', function() {
+    const down_btn = document.getElementById("down");
+    down_btn.addEventListener("click", function (){
+        window.scrollTo(0, document.body.scrollHeight);
+    });
 });
