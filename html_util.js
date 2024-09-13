1f07779c4b1319a4952b06caf612ea9c6981b560
AT3KCA <biliat3k@163.com>
2024-09-13 20:44:50 +0800

    Update
    JS Load DOM

--- html_util.js
+++ html_util.js
@@ -1,32 +1,34 @@
-window.addEventListener('load', function() {
-    const down_btn = document.getElementById("down")
-    if (down_btn == null){
-        return
+window.addEventListener("load",function(){
+    let down_btn = document.getElementById('down');
+    if (down_btn != null) {
+        down_btn.addEventListener('click', () => {
+            downBTN()
+        })
     }
-    down_btn.addEventListener("click", function (){
-        window.scrollTo(0, document.body.scrollHeight)
-    });
-});
+})
 
+function downBTN() {
+    window.scrollTo(0, document.body.scrollHeight)
+}
 
 
 let inp
 let count = 0
 let timer
 let isEnd = false
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     inp = document.getElementById('in');
-    if (!inp){
+    if (!inp) {
         return
     }
     inp.addEventListener('click', function () {
         count++;
-        if (count >= 6){
-            if(isEnd){
+        if (count >= 6) {
+            if (isEnd) {
                 return
             }
             isEnd = true;
-            inp.innerHTML = "<a target='_blank' href='/the-hidden.html'>AT3K_CA copyright</a>";
+            inp.innerHTML = "<a class='hover_b' target='_blank' href='/the-hidden.html'>AT3K_CA copyright</a>";
         }
         if (!timer) {
             timer = setTimeout(() => {
@@ -39,15 +41,45 @@ window.addEventListener('load', function() {
 });
 
 
-
-function msg_render() {
-    const msgDiv = document.getElementById('msg')
-    msgDiv.style.display = 'block'
+function isHtmlTag(line) {
+    let htmlTagPattern = /<(" "]*"|'[^']*'|[^'">])*>/
+    return htmlTagPattern.test(line);
 }
-function render_current(button){
-    let parent = button.closest('div');
-    parent.querySelectorAll('div').forEach(div => {
-        div.className = div.className.replace("hidden_div", "");
-    });
-    button.style = "display: none;"
+
+
+function load_text(name) {
+
+    const http = new XMLHttpRequest();
+    let url = "/" + name;
+    let div = document.getElementById("boxs");
+    div.innerHTML = "<p>尝试加载中</p>";
+    http.open("GET", url, true);
+    http.send();
+    http.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            div.innerHTML = ""
+            let ps = http.responseText.split("#End_HTP");
+            for (let i = 0; i < ps.length; i++) {
+                let box = document.createElement("div");
+                box.className = "box";
+
+                let ls = ps[i].split("\n");
+
+                for (let j = 0; j < ls.length; j++) {
+                    if (isHtmlTag(ls[j])) {
+                        box.innerHTML += ls[j]
+                    } else {
+                        if (ls[j].trim() === "") continue
+                        let ap = document.createElement("p");
+                        ap.innerText = ls[j]
+                        box.append(ap);
+                    }
+                }
+                div.append(box);
+            }
+        } else {
+            div.innerHTML = "<p>加载失败</p>"
+        }
+    }
+    console.log("loaded document:", name);
 }
